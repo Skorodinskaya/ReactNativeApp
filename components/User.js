45b462e5f0c6b557c3ef68839ae701ac3aa76700
Skorodinskaya <skorodinskaya01@gmail.com>
2021-09-11 00:48:50 +0300

    init commit

---
+++ components/User.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {StyleSheet} from 'react-native';
+import {View, Text, Button, TouchableOpacity} from 'react-native';
+
+const User = ({item}) => {
+    return <View>
+        <Text>{item.name}</Text>
+    </View>;
+};
+export default User;
+
+let styles = StyleSheet.create({});
---
+++ components/Users.js
@@ -0,0 +1,24 @@
+import React, {useEffect, useState} from 'react';
+import {FlatList, StyleSheet} from 'react-native';
+import {View, Text, Button, TouchableOpacity} from 'react-native';
+import {getUsers} from "../services/api.service";
+import User from "./User";
+
+const Users = () => {
+    const [users, setUsers] = useState();
+    useEffect(()=> {
+        getUsers().then(value => setUsers([...value]))
+    }, []);
+
+    return <View>
+<FlatList
+    data={users}
+    renderItem={({item}) => <User item={item}/>}
+    keyExtractor={item => '' + item.id}
+/>
+
+    </View>;
+};
+export default Users;
+
+let styles = StyleSheet.create({});
