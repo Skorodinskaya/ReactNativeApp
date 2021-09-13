3671f62db28136f6f7ee088b3b34f3e5a70e5f8d
Skorodinskaya <skorodinskaya01@gmail.com>
2021-09-13 22:29:50 +0300

    Some changes

---
+++ screens/UserDetails.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {StyleSheet} from 'react-native';
+import {View, Text, Button, TouchableOpacity} from 'react-native';
+
+const UserDetails = () => {
+    return <View>
+        <Text>UserDetails page</Text>
+    </View>;
+};
+export default UserDetails;
+
+let styles = StyleSheet.create({});
